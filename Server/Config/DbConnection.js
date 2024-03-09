a8d82f05f5c0ffe5b5b15741007e84cc443c9b08
Arif <115531149+whynotdev@users.noreply.github.com>
2024-03-10 04:19:01 +0530

    final

--- Server/Config/DbConnection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const databaseConnection = () => {
-  mongoose.connect(process.env.MONGO_URL_CON).then(() => {
-    console.log("Mongo db is connected successfully");
-  });
-};
-
-module.exports = { databaseConnection };
--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,127 @@
 import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./Components/LandingPage";
-import UserReg from "./Components/UserReg";
-import UserLogin from "./Components/UserLogin";
-import PatientDashboard from "./Pages/PatientsDashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Button } from "antd";
+import { Toaster } from "react-hot-toast";
+import Home from "./pages/Home";
+import { useSelector } from "react-redux";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
+import ApplyDoctor from "./pages/ApplyDoctor";
+import Notifications from "./pages/Notifications";
+import Userslist from "./pages/Admin/Userslist";
+import DoctorsList from "./pages/Admin/DoctorsList";
+import Profile from "./pages/Doctor/Profile";
+import BookAppointment from "./pages/BookAppointment";
+import Appointments from "./pages/Appointments";
+import DoctorAppointments from "./pages/Doctor/DoctorAppointments";
 
 function App() {
+  const { loading } = useSelector((state) => state.alerts);
   return (
-    <Router>
+    <BrowserRouter>
+      {loading && (
+        <div className="spinner-parent">
+          <div class="spinner-border" role="status"></div>
+        </div>
+      )}
+      <Toaster position="top-center" reverseOrder={false} />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<UserLogin/>} />
-        <Route path="/register" element={<UserReg />} />
-        <Route path="/Patients" element={<PatientDashboard/>} />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/apply-doctor"
+          element={
+            <ProtectedRoute>
+              <ApplyDoctor />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/notifications"
+          element={
+            <ProtectedRoute>
+              <Notifications />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/userslist"
+          element={
+            <ProtectedRoute>
+              <Userslist />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/doctorslist"
+          element={
+            <ProtectedRoute>
+              <DoctorsList />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/doctor/profile/:userId"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/book-appointment/:doctorId"
+          element={
+            <ProtectedRoute>
+              <BookAppointment />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/appointments"
+          element={
+            <ProtectedRoute>
+              <Appointments />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/doctor/appointments"
+          element={
+            <ProtectedRoute>
+              <DoctorAppointments />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
