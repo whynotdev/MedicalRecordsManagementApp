731cc41b2b2eaa55ace66a49796613da0fa7f7b3
Arif <115531149+whynotdev@users.noreply.github.com>
2024-03-14 23:57:09 +0530

    minor

--- client/src/components/BloodTest.js
+++ client/src/components/BloodTest.js
@@ -4,7 +4,7 @@ function BloodTest() {
   return (
     <>
       <div>
-        <h1 className="page-title">HealthTracking</h1>
+        <h1 className="page-title">HealthTrackings</h1>
         <hr/>
         {/* Overview */}
         <div>
--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -77,11 +77,11 @@ function Layout({ children }) {
       path: "/admin/doctorslist",
       icon: "ri-user-star-line",
     },
-    {
-      name: "Profile",
-      path: "/profile",
-      icon: "ri-user-line",
-    },
+    // {
+    //   name: "Profile",
+    //   path: "/profile",
+    //   icon: "ri-user-line",
+    // },
   ];
 
   const menuToBeRendered = user?.isAdmin
